730ad80ed81af03a1431db80e1d425288578a3de
gautamSH20 <gs841400@gmail.com>
2025-01-24 12:29:41 +0530

    new changes

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,18 +2,27 @@ import { EduIcon } from "../assets/project/Place";
 
 export const About = () => {
   return (
-    <div className="m-16 p-6  text-3xl  rounded-md shadow-md flex flex-col justify-center items-center ">
-      <p className="text-lg opacity-60">get to know me more</p>
-      <b>ABOUT ME !!!!</b>
-      <div className="flex w-90 items-cenetr gap-10">
-        <div className="h-[80vh] w-1/3 bg-black">hello</div>
-        <div className="w-2/3">
-          <section className="mt-2 w-[350px] h-[150px] bg-white rounded-xl shadow-md  flex flex-col flex-wrap justify-center items-center text-center border-black border-2 text-xl">
+    <div className="m-4 md:m-16 p-4 md:p-6 text-xl md:text-3xl rounded-md shadow-md flex flex-col justify-center items-center">
+      <p className="text-base md:text-lg opacity-60">get to know me more</p>
+      <b className="text-2xl md:text-4xl">ABOUT ME !!!!</b>
+
+      <div className="flex flex-col md:flex-row w-full items-center gap-4 md:gap-10 mt-6">
+        {/* Image Container */}
+        <div className="bg-[url(./assets/Me.jpg)] bg-center bg-cover bg-no-repeat h-[40vh] w-[20vw] md:h-[80vh] md:w-1/3 rounded-xl"></div>
+
+        {/* Content Container */}
+        <div className="w-full md:w-2/3 flex flex-col items-center">
+          {/* Education Card */}
+          <section className="mt-2 w-2/3 max-w-[250px] h-[150px] bg-white rounded-xl shadow-md flex flex-col justify-center items-center text-center border-black border-2 text-base md:text-xl p-2">
             <EduIcon />
-            <div className="mt-2">Eductaion:</div>
-            <p className="opacity-60">BCA:Bachelores of computer application</p>
+            <div className="mt-2">Education:</div>
+            <p className="opacity-60 text-sm md:text-base">
+              BCA: Bachelors of Computer Application
+            </p>
           </section>
-          <section className="mt-6 text-md text-gray-600">
+
+          {/* Description */}
+          <section className="mt-6 text-sm md:text-md text-gray-600 text-center px-4 md:px-0">
             Aspiring JavaScript Developer with a passion for front-end and
             full-stack web development. Proficient in React.js and Node.js,
             seeking an internship to apply technical skills in developing
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { GitIcon2, MailIcon2 } from "../assets/project/Place";
 import { About } from "./About";
 import { Contact } from "./Contatct";
 import { Nav } from "./Nav";
@@ -6,20 +7,31 @@ import { Skills } from "./Skills";
 
 export const Home = () => {
   return (
-    <div className="h-full bg-[#F1F1F1]">
+    <div className="h-full bg-[#F1F1F1] ">
       <Nav about="about" skills="skills" project="project" contact="contact" />
-      <section className="h-[100vh]  mt-0 flex  items-center justify-center gap-8">
-        <div className=" h-[75vh] bg-red-700 w-[75vh] rounded-full "></div>
-        <div className="w-1/3 text-[12vh]">
-          Gautam Sharma<br></br>
-          <span className="text-xl flex gap-8">
-            <button className="bg-black text-white p-2 rounded-xl">
-              github
+      <section className="min-h-screen h-auto md:h-[100vh] mt-0 flex flex-wrap items-center justify-center gap-8 p-4">
+        {/* Image Container */}
+        <div className="h-96 w-full md:h-[70vh] md:w-[70vh] rounded-full bg-[url(./assets/My.png)] bg-center bg-cover bg-no-repeat max-w-[70vh]"></div>
+
+        {/* Content Container */}
+        <div className="w-full md:w-1/3 flex flex-col justify-center items-center p-3 text-center border-2 border-black h-auto md:h-[75vh] mt-8 md:mt-0">
+          <p className="opacity-60 text-xl">Hi! I am</p>
+          <p className="text-4xl md:text-6xl mt-3 font-bold">Gautam Sharma</p>
+          <p className="font-bold text-gray-400 text-2xl md:text-4xl mt-4">
+            Web Developer
+          </p>
+          <span className="text-xl flex gap-4 md:gap-8 mt-4 flex-wrap justify-center">
+            <button className="bg-black text-white p-2 rounded-xl w-32">
+              GitHub
             </button>
-            <button className="bg-black text-white p-2 rounded-xl">
+            <button className="bg-black text-white p-2 rounded-xl w-32">
               Resume
             </button>
           </span>
+          <div className="flex gap-4 md:gap-8 mt-4">
+            <GitIcon2 />
+            <MailIcon2 />
+          </div>
         </div>
       </section>
       <section id="about">
